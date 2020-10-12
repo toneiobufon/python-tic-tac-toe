a42b69e6ac02c970fe29ec0c52608488ad7e687c
toneiojimz <ajimeneza77@gmail.com>
2020-10-11 21:21:21 -0700

    created player input to start game

--- TicTacToe.py
+++ TicTacToe.py
@@ -12,3 +12,28 @@ def display_board(board):
 #empty board
 test_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 display_board(test_board)
+
+#step2, write a function that can take a player input and assign their option
+# X or O, use while loop to ask until you get the right answer
+
+def player_input():
+    choice = ''
+
+    
+    while choice != 'X' and choice != 'O':
+        
+        choice = input('Player1, choose X or O:  ')
+        print('\n')
+    player1 = choice
+    
+    if player1 == 'X':
+        player2 = 'O'
+
+    else:
+        player2 = 'X'
+    print('player 1 :', player1,   '  player 2 :', player2)
+    
+    
+    return player1, player2
+
+player_input() 
